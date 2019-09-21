f934f11f7fe009d46f5390968f71e51c7c2a1b78
Vaibhav Sharma <vsvaibhav2016@gmail.com>
2019-09-21 13:15:23 +0530

    creating loging screen

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -16,9 +16,38 @@ class _LoginPageState extends State<Login> {
             mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              FlutterLogo(size: 150)
+              FlutterLogo(size: 150),
+              SizedBox(height: 50),
+              googleSigninBtn(),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+
+  Widget googleSigninBtn() {
+    return OutlineButton(
+      splashColor: Colors.grey,
+      onPressed: () {
+
+      },
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+      highlightElevation: 0,
+      borderSide: BorderSide(color: Colors.grey),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image(image: AssetImage("assets/google_logo.png"),height: 35.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text('Sign in with Google',style: TextStyle(fontSize: 20,color: Colors.grey)),
+            )
+          ],
         ),
       ),
     );
---
+++ lib/widgets/google-sign.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+/* class GoogleSignIn extends 
+
+
+class Login extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+} */
